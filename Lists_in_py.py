4fa41ef971f9f716f23d8e234f8fa9ce1e0b8b36
ialikh72 <ialikh.w1@gmail.com>
2026-03-18 21:34:22 +0500

    Add project files.

---
+++ Lists_in_py.py
@@ -0,0 +1,8 @@
+movies=[]
+movie1=input("enter movie name:")
+movie2=input("enter movie name:")
+movie3=input("enter movie name:")
+movies.append(movie1)
+movies.append(movie2)
+movies.append(movie3)
+print(movies)
